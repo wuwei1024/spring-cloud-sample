7c678f7466316531d89b91eb23e93452dcb9f7a2
wuwei <1472101076@qq.com>
2018-04-26 15:22:51 +0800

    添加配置管理服务config

---
+++ config/src/main/java/com/test/ConfigApplication.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author: wuwei
+ * @date: 2018/4/26 15:04
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
